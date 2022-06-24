e591eb8d390e134d16a27030a6d90cc8e29433e1
unreal-kz <ukibas.b@gmail.com>
2022-06-24 19:32:23 +0300

    main.go file has been implemented

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Printf("domain, hasMX, hasSPF, sprRecord, hasDMARC, dmarcRecord\n")
+
+	for scanner.Scan() {
+		checkDomain(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal("Error: could not read from input: %v\n", err)
+	}
+
+}
+
+func checkDomain(domain string) {
+	var hasMX, hasSPF, hasDMARC bool
+	var spfRecord, dmarcRecord string
+
+	mxRecords, err := net.LookupMX(domain)
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+	if len(mxRecords) > 0 {
+		hasMX = true
+	}
+	spfRecords, err := net.LookupTXT(domain)
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+	for _, rec := range spfRecords {
+		if strings.HasPrefix(rec, "v=spf1") {
+			hasSPF = true
+			spfRecord = rec
+			break
+		}
+	}
+
+	dmarRecords, err := net.LookupTXT("_dmarc." + domain)
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+	for _, rec := range dmarRecords {
+		if strings.HasPrefix(rec, "v=DMARC1") {
+			hasDMARC = true
+			dmarcRecord = rec
+			break
+		}
+	}
+
+	fmt.Printf("%v, %v, %v, %v, %v, %v\n", domain, hasMX, hasSPF, spfRecord, hasDMARC, dmarcRecord)
+}
